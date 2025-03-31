39a50f8bb1980398b402efb76b02b5cd111214ba
HarmonyH27 <159847908+HarmonyH27@users.noreply.github.com>
2025-03-31 14:46:58 -0400

    Add form to JS
    Create form for users to RSVP to event

--- index.js
+++ index.js
@@ -2,12 +2,43 @@
 // Dark Toggle
 
 let themeButton = document.querySelector('#theme-button');
+let rsvpButton = document.querySelector('#rsvp-button');
+
+let count = 3;
+
+const participantBox = document.querySelector('.rsvp-participants');
+
 
 const toggleDarkMode = () => {
     document.body.classList.toggle("dark-mode");
-    console.log("button clicked!");
+}
+
+const addParticipant = (event) =>{
+    
+    let name = document.querySelector('#name').value;
+    let city = document.querySelector('#city').value;
+
+
+    if((name == '') || (city == '')){
+        return;
+    }else{
+        const rsvpInput = document.createElement("p");
+        rsvpInput.innerText = "🎟️" + name + " from " +  city + " has RSVP'd";   
+        participantBox.appendChild(rsvpInput);
+
+        //Clear field
+        document.querySelector('#name').value = '';
+        document.querySelector('#city').value = '';
+        document.querySelector('#email').value = '';
+
+    }
+
+
+    event.preventDefault();
+
 }
 
 themeButton.addEventListener('click', toggleDarkMode);
+rsvpButton.addEventListener('click', addParticipant)
 
 
